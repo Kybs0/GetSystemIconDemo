e4fd67000204c2e716d3230b3e6e41574a138fd3
yudong <yudong@cvte.com>
2020-08-04 21:14:05 +0800

    添加项目文件。

---
+++ GetSystemIconDemo/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
+using Size = System.Drawing.Size;
+
+namespace GetSystemIconDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var text = InputTextBox.Text;
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return;
+            }
+
+            text = text.Replace(@"""", string.Empty);
+            if (File.Exists(text))
+            {
+                var filePath = text;
+                //方案1 只能获取小图标
+                //var fileIcon = SystemIconHelper.GetFileIcon(filePath);
+                //var imageSource = fileIcon.ToImageSource();
+                var image = new Image();
+                //方案2 获取大图标
+                image.Source = ThumbnailUtil.GetLargeIcon(filePath);
+                ImagesPanel.Children.Add(image);
+            }
+            else if (Directory.Exists(text))
+            {
+                var folderPath = text;
+                var folderIcon = SystemIconHelper.GetFileIcon(folderPath);
+                var imageSource = folderIcon.ToImageSource();
+                var image = new Image();
+                image.Source = imageSource;
+                ImagesPanel.Children.Add(image);
+            }
+        }
+    }
+}
---
+++ GetSystemIconDemo/SystemIconHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSystemIconDemo
+{
+    public static class SystemIconHelper
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public IntPtr iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+
+        }
+        /// <summary>
+        /// 返回系统设置的图标
+        /// </summary>
+        /// <param name="pszPath">文件路径 如果为""  返回文件夹的</param>
+        /// <param name="dwFileAttributes">0</param>
+        /// <param name="psfi">结构体</param>
+        /// <param name="cbSizeFileInfo">结构体大小</param>
+        /// <param name="uFlags">枚举类型</param>
+        /// <returns>-1失败</returns>
+        [DllImport("shell32.dll")]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+        public enum SHGFI
+        {
+            SHGFI_ICON = 0x100,
+            SHGFI_LARGEICON = 0x0,
+            SHGFI_USEFILEATTRIBUTES = 0x10
+        }
+
+        /// <summary>
+        /// 获取文件图标
+        /// </summary>
+        /// <param name="filePath">文件全路径</param>
+        /// <returns>图标</returns>
+        public static Icon GetFileIcon(string filePath)
+        {
+            SHFILEINFO shfileinfo = new SHFILEINFO();
+            IntPtr iconIntPtr = SHGetFileInfo(filePath, 0, ref shfileinfo, (uint)Marshal.SizeOf(shfileinfo), (uint)(SHGFI.SHGFI_USEFILEATTRIBUTES| SHGFI.SHGFI_LARGEICON |SHGFI.SHGFI_ICON ));
+            if (iconIntPtr.Equals(IntPtr.Zero))
+                return null;
+            Icon fileIcon = System.Drawing.Icon.FromHandle(shfileinfo.hIcon);
+            return fileIcon;
+        }
+
+        /// <summary>
+        /// 获取文件夹图标
+        /// </summary>
+        /// <returns>图标</returns>
+        public static Icon GetDirectoryIcon()
+        {
+            SHFILEINFO shfileinfo = new SHFILEINFO();
+            IntPtr fileInfo = SHGetFileInfo(@"", 0, ref shfileinfo, (uint)Marshal.SizeOf(shfileinfo), (uint)( SHGFI.SHGFI_LARGEICON|SHGFI.SHGFI_ICON));
+            if (fileInfo.Equals(IntPtr.Zero))
+                return null;
+            Icon directoryIcon = System.Drawing.Icon.FromHandle(shfileinfo.hIcon);
+            return directoryIcon;
+        }
+    }
+}
